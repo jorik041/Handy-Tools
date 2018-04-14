07aeac047f9fc240eb1924954d403a919068da82
Ryan Miller <ryan@reptoidgames.com>
2018-04-14 15:46:34 -0400

    added rename and replace utilities

---
+++ Editor/AdvancedRename.cs
@@ -0,0 +1,129 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AdvancedRename : MonoBehaviour {
+
+	public static void RenameNames(string newName, bool isSuffix, bool isPrefix, bool numberItems, bool sortInHierarchy, int padding, int startNum, int incrementBy){
+		int indexCount = startNum;
+		string padFormat = "";
+		for (int padNum = 0; padNum < padding; padNum++) {
+			padFormat += "0";
+		}
+
+		for (int itemIndex = 0; itemIndex < Selection.transforms.Length; itemIndex ++) {
+
+			string thisItemName = Selection.transforms[itemIndex].name;
+
+			// add name 
+			if (!isPrefix && !isSuffix) {
+				Debug.Log("Rename without suffix or prefix");
+				Selection.transforms[itemIndex].name = newName;
+			}
+			else {
+				if (isPrefix) {
+					Debug.Log("rename with Prefix");
+					Selection.transforms[itemIndex].name = newName + thisItemName;
+				}
+				if (isSuffix) {
+					Debug.Log("rename with Suffix");
+					Selection.transforms[itemIndex].name = thisItemName + newName;
+				}
+			}
+
+			if (numberItems) {
+				// add trailing number
+				Selection.transforms[itemIndex].name += indexCount.ToString(padFormat);
+				indexCount += incrementBy;
+			}
+		}
+
+		if (sortInHierarchy) {
+			for (int itemIndex = 0; itemIndex < Selection.transforms.Length; itemIndex ++) {
+				Selection.transforms[itemIndex].SetSiblingIndex(itemIndex);
+			}
+		}
+	}
+}
+public class RenameMaster : EditorWindow{
+	public enum NameOperation
+	{
+		NewName,
+		AddPrefix,
+		AddSuffix
+	}
+	public NameOperation nameOperation;
+	
+	[MenuItem("Window/Advanced Rename")]
+	public static void Init()
+	{
+		// Get existing open window or if none, make a new one:
+		RenameMaster window = (RenameMaster)EditorWindow.GetWindow(typeof(RenameMaster));
+		
+		#if UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0
+		window.title = "Rename";
+		#else
+		window.titleContent = new GUIContent ("Rename");
+		#endif
+		window.Show();
+	}
+	string rename = "";
+	int numberPadding = 4;
+	bool numberItems = true;
+	bool sortInHierarchy = true;
+	int startNumber = 0;
+	int incrementBy = 1;
+	
+	void OnGUI()
+	{
+		GUILayout.Label("Rename Multiple Files", EditorStyles.boldLabel);
+		GUILayout.Label("Note: Undo is not currently supported. Save your scene first!");
+		rename = EditorGUILayout.TextField("New Name: ", rename);
+
+		nameOperation = (NameOperation)EditorGUILayout.EnumPopup("Operation: ", nameOperation);
+
+		numberItems = EditorGUILayout.Toggle("Number Items: ", numberItems);
+
+		if (numberItems) {
+			numberPadding = Mathf.Clamp(EditorGUILayout.IntField("Number Padding: ", numberPadding), 0, 10000);
+			startNumber = EditorGUILayout.IntField("Start Number: ", startNumber);
+			incrementBy = Mathf.Clamp(EditorGUILayout.IntField("Increment by (step): ", incrementBy), 1, 100000);
+			sortInHierarchy = EditorGUILayout.Toggle("Sort in Hierarchy: ", sortInHierarchy);
+		}
+
+		if (Selection.transforms.Length > 0) {
+			if (GUILayout.Button("Rename " + Selection.transforms.Length + " Item(s)"))
+			{
+				AdvancedRename.RenameNames(rename, nameOperation == NameOperation.AddSuffix, nameOperation == NameOperation.AddPrefix, numberItems, sortInHierarchy, numberPadding, startNumber, incrementBy);
+			}
+		}
+
+		string sample = "";
+
+		if (nameOperation == NameOperation.AddPrefix) {
+			sample = rename + "[name]";
+		}
+		else if (nameOperation == NameOperation.AddSuffix) {
+			sample = "[name]" + rename;
+		}
+		else if (nameOperation == NameOperation.NewName) {
+			sample = rename;
+		}
+
+		if (numberItems) {
+			string padFormat = "";
+			for (int padNum = 0; padNum < numberPadding; padNum++) {
+				padFormat += "0";
+			}
+			sample += startNumber.ToString(padFormat);
+		}
+
+		GUILayout.Space (10);
+
+		GUILayout.Label("Preview: " + sample);
+	}
+}
+
+#endif
---
+++ Editor/AdvancedReplace.cs
@@ -0,0 +1,145 @@
+﻿#if UNITY_EDITOR
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Handy {
+
+	public class AdvancedReplace : EditorWindow {
+		
+		public enum SearchType
+		{
+			SearchByName = 0,
+			SearchByTag = 1
+		}
+		public SearchType searchType;
+		
+		[MenuItem("Window/Advanced Replace")]
+		public static void Init()
+		{
+			// Get existing open window or if none, make a new one:
+			AdvancedReplace window = (AdvancedReplace)EditorWindow.GetWindow(typeof(AdvancedReplace));
+			
+			#if UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0
+			window.title = "Replace Objects";
+			#else
+			window.titleContent = new GUIContent ("Replace Objects");
+			#endif
+			window.Show();
+		}
+		
+		string find = "";
+		GameObject replace;
+		bool selected = true;
+		bool byTag = false;
+		
+		void OnGUI()
+		{
+			GUILayout.Label("Find and Replace with GameObjects", EditorStyles.boldLabel);
+			GUILayout.Label("Note: Undo is not currently supported. Save your scene first!");
+			find = EditorGUILayout.TextField("Search for: ", find);
+			searchType = (SearchType)EditorGUILayout.EnumPopup("Search by: ", searchType);
+			replace = EditorGUILayout.ObjectField("Replace with: ", replace, typeof(GameObject), true) as GameObject;
+			selected = EditorGUILayout.Toggle("Search within selection: ", selected);
+			
+			if (GUILayout.Button("Find and Replace GameObjects"))
+			{
+				byTag = searchType == SearchType.SearchByTag;
+				FindReplaceObjects(find, replace, selected, byTag);
+			}
+			
+			GUILayout.Space (10);
+			
+			if (Selection.transforms.Length > 0 && find != "") {
+				GUILayout.BeginVertical ();
+				
+				string replaceText = "<select a prefab>";
+				if (replace != null) {
+					replaceText = replace.name;
+				}
+				
+				GUILayout.Label ("Replace these with " + replaceText, EditorStyles.boldLabel);
+				
+				foreach (Transform t in Selection.transforms) {
+					GUILayout.Label (t.gameObject.name);
+				}
+				GUILayout.EndVertical ();
+			}
+		}
+
+		public static void FindReplaceNames(string find, string replace, bool withinSelection)
+		{
+
+			if (withinSelection)
+			{
+				if (Selection.gameObjects.Length > 0)
+				{
+					Undo.RecordObjects(Selection.gameObjects, "Find Replace Names");
+					foreach (GameObject gO in Selection.gameObjects)
+					{
+						gO.name = gO.name.Replace(find, replace);
+					}
+				}
+			}
+			else
+			{
+				//Undo.RecordObjects(Selection.gameObjects, "Find Replace Names");
+				foreach (GameObject gO in FindObjectsOfType<GameObject>())
+				{
+					gO.name = gO.name.Replace(find, replace);
+				}
+			}
+		}
+
+		public static void FindReplaceObjects(string find, GameObject replace, bool withinSelection, bool byTag)
+		{
+			if (withinSelection)
+			{
+				if (Selection.gameObjects.Length > 0)
+				{
+					Undo.RecordObjects(Selection.gameObjects, "Find Replace Objects");
+					foreach (GameObject gO in Selection.gameObjects)
+					{
+						if (!byTag && gO.name.Contains(find))
+						{
+							GameObject newObj = Instantiate(replace, gO.transform.position, gO.transform.rotation) as GameObject;
+							newObj.transform.parent = gO.transform.parent;
+							newObj.name = newObj.name.Replace("(Clone)", "");
+							DestroyImmediate(gO);
+						}
+						else if (byTag && gO.tag.Contains(find))
+						{
+							GameObject newObj = Instantiate(replace, gO.transform.position, gO.transform.rotation) as GameObject;
+							newObj.transform.parent = gO.transform.parent;
+							newObj.name = newObj.name.Replace("(Clone)", "");
+							DestroyImmediate(gO);
+						}
+					}
+				}
+			}
+			else
+			{
+				//Undo.RecordObjects(Selection.gameObjects, "Find Replace Objects");
+				foreach (GameObject gO in FindObjectsOfType<GameObject>())
+				{
+					if (!byTag && gO.name.Contains(find))
+					{
+						GameObject newObj = Instantiate(replace, gO.transform.position, gO.transform.rotation) as GameObject;
+						newObj.transform.parent = gO.transform.parent;
+						newObj.name = newObj.name.Replace("(Clone)", "");
+						DestroyImmediate(gO);
+					}
+					else if (byTag && gO.tag.Contains(find))
+					{
+						GameObject newObj = Instantiate(replace, gO.transform.position, gO.transform.rotation) as GameObject;
+						newObj.transform.parent = gO.transform.parent;
+						newObj.name = newObj.name.Replace("(Clone)", "");
+						DestroyImmediate(gO);
+					}
+				}
+			}
+		}
+	}
+}
+#endif
